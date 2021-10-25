fee9ececc4769f57bb8f0e9f6fbe6dea1736e319
Daniel M. Matongo <m_matongo@icloud.com>
2021-10-25 14:47:57 +0200

    add sql containing the data

---
+++ data.sql
@@ -0,0 +1,6 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', '10.2', '1', '0');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-11-15', '8', '1', '2');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', '2020-01-07', '15.04', '0', '1');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', '2017-05-12', '11', '1', '5');
