f18db11cb9478d21f20278436673b8047b2e7395
Daniel M. Matongo <m_matongo@icloud.com>
2021-10-26 14:21:20 +0200

    update queries

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
---Queries that provide answers to the questions from all projects.
+/* Queries that provide answers to the questions from all projects. */
 
 -- Find all animals whose name ends in "mon". 
 SELECT name FROM animals WHERE name LIKE '%mon';
@@ -9,10 +9,10 @@ SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-3
 -- List the name of all animals that are neutered and have less than 3 escape attempts. 
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
 
--- List date of birth of all animals named either "Agumon" or "Pikachu". 
+-- List date of birth of all animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 
--- List name and escape attempts of animals that weigh more than 10.5kg 
+-- List name and escape attempts of animals that weigh more than 10.5kg
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 -- Find all animals that are neutered. 
@@ -24,7 +24,7 @@ SELECT name FROM animals WHERE name != 'Gabumon';
 -- Find all animals with a weight between 10.4kg and 17.3kg & animals with weight that equals precisely 10.4kg or 17.3kg). 
 SELECT name, weight_kg FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
--- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. 
+/* Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. 
 Then roll back the change and verify that species columns went back to the state before tranasction. */
 BEGIN;
 UPDATE animals SET species = 'unspecified';
@@ -33,7 +33,10 @@ ROLLBACK;
 -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
+
+-- For all animals with null species, set species to pokemon.
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+
 COMMIT;
 
 -- Inside a transaction delete all records in the animals table, then roll back the transaction.
@@ -77,4 +80,13 @@ SELECT COUNT(*) FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+SELECT
+	species,
+	AVG(escape_attempts)
+FROM
+	animals
+WHERE
+	date_of_birth BETWEEN '1990-01-01'
+	AND '2000-12-31'
+GROUP BY
+	species;
