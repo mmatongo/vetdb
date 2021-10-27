103d3c769843b54edddabd8c760cef4f9013a4c5
Daniel M. Matongo <m_matongo@icloud.com>
2021-10-27 11:59:45 +0200

    fixes

--- data.sql
+++ data.sql
@@ -1,17 +1,4 @@
 /* Populate database with sample data. */
-<<<<<<< Updated upstream
-
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', '10.2', '1', '0');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-11-15', '8', '1', '2');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', '2020-01-07', '15.04', '0', '1');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', '2017-05-12', '11', '1', '5');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Charmander', '2020-02-08', '-11', '0', '0');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Plantmon', '2022-11-15', '-5.7', '1', '2');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Squirtle', '1993-01-07', '-12.13', '0', '3');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Angemon', '2005-06-12', '-45', '1', '1');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Boarmon', '2005-06-7', '20.4', '1', '7');
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Angemon', '1998-10-13', '17', '1', '3');
-=======
 INSERT INTO animals (
   name, date_of_birth, weight_kg, neutered, 
   escape_attempts
@@ -258,5 +245,3 @@ GROUP BY
   owners.full_name 
 ORDER BY 
   COUNT(animals.name) DESC;
-
->>>>>>> Stashed changes
--- schema.sql
+++ schema.sql
@@ -1,19 +1,17 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals(
-   id INT GENERATED ALWAYS AS IDENTITY, 
-   name VARCHAR(100),
-   date_of_birth DATE,
-   escape_attempts INT,
-   neutered BOOLEAN,
-   weight_kg DECIMAL,
-   PRIMARY KEY(id)
+CREATE TABLE owners (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	full_name VARCHAR(100),
+	age INT,
+	PRIMARY KEY (id)
 );
 
-<<<<<<< Updated upstream
-/* Add a table for spiecies */
-ALTER TABLE animals ADD COLUMN species VARCHAR(100);
-=======
+CREATE TABLE species (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100),
+	PRIMARY KEY (id)
+);
+
 CREATE TABLE animals (
 	id INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(100),
@@ -27,4 +25,3 @@ CREATE TABLE animals (
 	--Add column owner_id which is a foreign key referencing the owners table
 	owner_id INT REFERENCES owners (id)
 );
->>>>>>> Stashed changes
